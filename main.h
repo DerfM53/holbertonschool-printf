913a5c5da4a60927ab32d8b3c06912ca54b0ffbc
Your Name <you@example.com>
2024-07-22 22:12:05 +0200

    _printftest

--- main.h
+++ main.h
@@ -1,10 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+typedef struct prif
+{
+	char *prif;	// Utilisé pour stocker un spécificateur de format (par exemple, "%d", "%s", etc.)
+	void (*f)(va_list args); // Pointeur vers une fonction qui prendra va_list comme argument
+} prif_t;
+
+/* Prototype de la function */
+
 int _printf(const char *format, ...);
 
-
+/* déclaration des fonctions pour chaque spécificateurs */
+void print_char(va_list args);
+void print_string(va_list args)
+void print_percent(va_list args)
 
 
 
