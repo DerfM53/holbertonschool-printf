2d2e305b1da2ff95520556dfe78ebdcef9f9517f
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-22 22:43:46 +0200

    correction de la syntaxe Betty Holberton pour fichier _print_number.c et _printf.c

--- _print_number.c
+++ _print_number.c
@@ -9,16 +9,17 @@
  */
 int _print_number(int n)
 {
-    int count = 0;
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-        count++;
-    }
-    if (n / 10)
-        count += _print_number(n / 10);
-    _putchar(n % 10 + '0');
-    count++;
-    return count;
+	int count = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		count++;
+	}
+	if (n / 10)
+		count += _print_number(n / 10);
+	_putchar(n % 10 + '0');
+	count++;
+	return (count);
 }
--- _printf.c
+++ _printf.c
@@ -4,51 +4,51 @@
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    const char *p;
+	va_list args;
+	int count = 0;
+	const char *p;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (p = format; *p; p++)
-    {
-        if (*p == '%')
-        {
-            p++;
-            if (*p == 'c')
-            {
-                char c = va_arg(args, int);
-                _putchar(c);
-                count++;
-            }
-            else if (*p == 's')
-            {
-                char *s = va_arg(args, char *);
-                while (*s)
-                {
-                    _putchar(*s++);
-                    count++;
-                }
-            }
-            else if (*p == '%')
-            {
-                _putchar('%');
-                count++;
-            }
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == 'c')
+			{
+				char c = va_arg(args, int);
+				_putchar(c);
+				count++;
+			}
+			else if (*p == 's')
+			{
+				char *s = va_arg(args, char *);
+				while (*s)
+				{
+					_putchar(*s++);
+					count++;
+				}
+			}
+			else if (*p == '%')
+			{
+				_putchar('%');
+				count++;
+			}
 			else if (*p == 'd')
 			{
 				int d = va_arg(args, int);
 				count += _print_number(d);
 			}
-        }
-        else
-        {
-            _putchar(*p);
-            count++;
-        }
-    }
+		}
+		else
+		{
+			_putchar(*p);
+			count++;
+		}
+	}
 
-    va_end(args);
+	va_end(args);
 
-    return count;
+	return (count);
 }
