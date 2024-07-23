e5a7e41f588cf5860c28783af9f89faeb9dae305
Your Name <you@example.com>
2024-07-23 21:26:58 +0200

    correct files

--- fred/_print_number.c
+++ fred/_print_number.c
@@ -1,11 +1,14 @@
 #include "main.h"
 #include <unistd.h>
-
 /**
- * _print_number - affiche un entier
- * @n: nombre entier à afficher
+ * _print_number - Affiche un entier en base décimale.
+ * @n: Le nombre entier à afficher.
  *
- * Return: le nombre de caractères imprimés
+ * Cette fonction affiche le nombre entier fourni en utilisant la fonction
+ * `_putchar` pour chaque chiffre. Si le nombre est négatif, elle affiche
+ * le signe '-' avant de traiter la valeur absolue du nombre.
+ *
+ * Return: Le nombre de caractères imprimés.
  */
 int _print_number(int n)
 {
@@ -20,51 +23,84 @@ int _print_number(int n)
 	if (n / 10)
 		count += _print_number(n / 10);
 	_putchar(n % 10 + '0');
-	count++;
-	return (count);
+	return (count + 1);
 }
-
 /**
- * _print_unsigned_number - affiche un entier
- * @n: nombre entier à afficher
+ * _print_unsigned_number - Affiche un entier non signé en base décimale.
+ * @n: Le nombre entier non signé à afficher.
  *
- * Return: le nombre de caractères imprimés
+ * Cette fonction affiche le nombre entier non signé fourni en utilisant la
+ * fonction `_putchar` pour chaque chiffre. Elle gère les chiffres de manière
+ * récursive.
+ *
+ * Return: Le nombre de caractères imprimés.
  */
 int _print_unsigned_number(unsigned int n)
 {
-
 		int count = 0;
 
 		if (n / 10)
 
 			count += _print_unsigned_number(n / 10);
 	_putchar(n % 10 + '0');
-	count++;
-	return (count);
+	return (count + 1);
 }
+/**
+ * _print_hexadecimal - Imprime un nombre non signé en hexadécimal.
+ * @n: Le nombre non signé à imprimer.
+ * @uppercase: Si non zéro, utilise des lettres majuscules;
+ * sinon, des lettres minuscules.
+ * Cette fonction imprime le nombre fourni en utilisant le format hexadécimal
+ * (base 16). La récursion est utilisée pour traiter
+ * chaque chiffre du nombre.
+ * Les lettres hexadécimales sont choisies
+ * en fonction de la valeur de `uppercase`.
+ * Return: Le nombre de caractères imprimés.
+ */
+int _print_hexadecimal(unsigned long n, int uppercase)
+{
+	const char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
 
+	if (n / 16)
 
-int _print_hexadecimal(unsigned int u, int uppercase)
-
+		_print_hexadecimal(n / 16, uppercase);
+	_putchar(digits[n % 16]);
+	return (0);
+}
+/**
+ * _print_octal - Imprime un nombre entier non signé en octal (base 8).
+ * @n: Le nombre entier non signé à imprimer.
+ *
+ * Cette fonction imprime le nombre fourni en utilisant le format octal
+ * (base 8). La récursion est utilisée pour traiter chaque chiffre du nombre.
+ *
+ * Return: Le nombre de caractères imprimés.
+ */
+int _print_octal(unsigned int n)
+{
+		if (n / 8)
+			_print_octal(n / 8);
+		_putchar(n % 8 + '0');
+		return (0);
+}
+/**
+ * _print_string - Imprime une chaîne de caractères.
+ * @s: La chaîne de caractères à imprimer.
+ *
+ * Cette fonction affiche chaque caractère de la chaîne fournie en utilisant
+ * la fonction `_putchar`. La chaîne est affichée jusqu'à
+ * ce que le caractère de fin de chaîne (`\0`) soit rencontré.
+ *
+ * Return: Le nombre de caractères imprimés.
+ */
+int _print_string(const char *s)
 {
 	int count = 0;
-	char digit;
 
-	if(u / 16)
+	while (*s)
 	{
-		count += _print_hexadecimal(u / 16, uppercase);
+		_putchar(*s++);
+		count++;
 	}
-	digit = u % 16;
-	if (digit < 10)
-	{
-		_putchar(digit + '0');
-	}
-	else
-	{
-		_putchar(digit - 10 + (uppercase ? 'A' : 'a'));
-	}
-	count++;
 	return (count);
 }
-
-
--- fred/main.h
+++ fred/main.h
@@ -6,7 +6,8 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int _print_number(int n);
 int _print_unsigned_number(unsigned int n);
-int _print_hexadecimal(unsigned int, int uppercase);
-
+int _print_hexadecimal(unsigned long n, int uppercase);
+int _print_octal(unsigned int n);
+int _print_string(const char *s);
 
 #endif
