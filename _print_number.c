dc1178e60f58c60cd63cf682a3f2400d50db6ff8
DerfM53 <frederic.mieuzet@gmail.com>
2024-07-23 11:24:16 +0200

    réorganisation des fichiers, fonction _print_unsigned_number et _putchar dans fichier _print_number

--- _print_number.c
+++ _print_number.c
@@ -23,3 +23,32 @@ int _print_number(int n)
 	count++;
 	return (count);
 }
+/**
+ * _print_unsigned_number - affiche un entier
+ * @n: nombre entier à afficher
+ *
+ * Return: le nombre de caractères imprimés
+ */
+int _print_unsigned_number(unsigned int n)
+{
+	int count = 0;
+
+	if (n / 10)
+		count += _print_unsigned_number(n / 10);
+	_putchar(n % 10 + '0');
+	count++;
+
+	return (count);
+}
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
